47f6708a02c53be3af1855e60c444893d9cd38df
ThomasanCRZ <Thomas.cruz@outlook.fr>
2025-02-19 20:17:15 +0100

    ADD footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/imgs/logo.png"
 function Footer() {
     const currentYear = new Date().getFullYear();
   return (
-    <div className="flex flex-col items-center mt-30 gap-1">
+    <div className="flex flex-col items-center mt-30 mb-3 gap-1">
         <img className="w-40" src={Logo} alt="Pokédev" />
         <p> Copyright &copy; {currentYear} Kaiodev</p>
     </div>
