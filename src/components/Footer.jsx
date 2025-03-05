5fc250498d948f909b878fcab32731d6477955f0
ThomasanCRZ <Thomas.cruz@outlook.fr>
2025-03-05 19:18:39 +0100

    Add link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ import Logo from "../assets/imgs/logo.png"
 
 function Footer() {
     const currentYear = new Date().getFullYear();
+    const urlKaiodev = "https://kaiodev.com/";
   return (
     <div className="flex flex-col items-center mt-30 mb-3 gap-1">
         <img className="w-40" src={Logo} alt="Pokédev" />
-        <p> Copyright &copy; {currentYear} Kaiodev</p>
+        <p> Copyright &copy; {currentYear} <a href={urlKaiodev}>Kaiodev</a></p>
     </div>
   )
 }
